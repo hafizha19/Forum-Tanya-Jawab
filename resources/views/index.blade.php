df2903812ac48ee0a8bb3bebef80dd49d34ca3be
hafizha19 <hafizha.kiromimz@gmail.com>
2020-08-13 22:02:38 +0800

    Add index before migrate

---
+++ resources/views/index.blade.php
@@ -0,0 +1,39 @@
+@include('items.navbar')
+
+<!-- Masthead-->
+<header class="masthead">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center justify-content-center text-center">
+            <div class="col-lg-10 align-self-end">
+                <h1 class="text-uppercase text-white font-weight-bold">Forum Tanya Jawab Terinteraktif
+                </h1>
+                <hr class="divider my-4" />
+            </div>
+            <div class="col-lg-8 align-self-baseline">
+                <blockquote class="blockquote text-white-75 font-weight-light mb-5">
+                    <p class="mb-0">Belajar dari kemarin, hidup untuk sekarang, berharap untuk besok. Hal yang paling penting adalah jangan berhenti bertanya.</p>
+                    <footer class="blockquote-footer">Albert Einstein</footer>
+                  </blockquote></p>
+                <a class="btn btn-primary btn-xl js-scroll-trigger" href="#pertanyaan">Temukan Jawaban Pertanyaanmu</a>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+<!-- Pertanyaan-->
+<section class="page-section" id="pertanyaan">
+    <div class="container">
+        <h2 class="text-center mt-0">Daftar Pertanyaan</h2>
+        <hr class="divider my-4" />
+        <div class="row">
+            <div class="col-md-3 text-center">
+                <div class="mt-5">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+@include('items.footer')
---
+++ resources/views/items/navbar.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Forum Tanya Jawab</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic"
+        rel="stylesheet" type="text/css" />
+    <!-- Third party plugin CSS-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css"
+        rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">Forum Tanya Jawab</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#pertanyaan">Punya Pertanyaan?</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
